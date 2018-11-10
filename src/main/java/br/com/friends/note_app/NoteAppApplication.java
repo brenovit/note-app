ca5f44b692a9cba69cffdc39eb4db2838463173a
Breno Nunes <breno.arantes97@gmail.com>
2018-11-09 21:41:14 -0300

    Instalação do Spring
    Instalação do Framework Spring;
    SpringBoot, SpringCouchbase, JPA; WEB; Security;

---
+++ src/main/java/br/com/friends/note_app/NoteAppApplication.java
@@ -0,0 +1,34 @@
+package br.com.friends.note_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class NoteAppApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(NoteAppApplication.class, args);
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:i18n/messages");
+		messageSource.setCacheSeconds(0);
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.setValidationMessageSource(messageSource());
+		return localValidatorFactoryBean;
+	}
+}
